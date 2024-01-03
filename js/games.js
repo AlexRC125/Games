8a5278a01d598d098e8027f8ca9aef81cc1b4788
Alex <125aramoscortes@frhsd.com>
2024-01-03 08:33:44 -0500

    changes 1

---
+++ js/games.js
@@ -0,0 +1,28 @@
+
+var height = 6; //number of guesses
+var width = 5; //length of guesses
+
+var row = 0; //current guess (attempt #)
+var col = 0; //current letter for that attempt
+
+var gameOver = false;
+var word = 'SQUID'
+
+window.onload = function(){
+    intialize();
+}
+
+function intialize(){
+
+    // Create the board
+    for (let r = 0; r < height; r++){
+        //<span id="0-0"> <class="tile">P<p/span>
+        for (let c = 0; c < width; c++){
+            let tile = document.createElement("span")
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "p"
+            document.getElementById("board").appendChild(tile)
+        }
+    }
+}
