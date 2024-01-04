d942685a9150770218d753c15a2f0632ce0fc114
Alex <125aramoscortes@frhsd.com>
2024-01-04 09:49:25 -0500

    changes 4

--- js/games.js
+++ js/games.js
@@ -21,8 +21,59 @@ function intialize(){
             let tile = document.createElement("span")
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
-            tile.innerText = "P";
+            tile.innerText = "";
             document.getElementById("board").appendChild(tile)
         }
+    }
+    // Listen for Key Press
+    document.addEventListener("keyup", (e) =>{
+        if (gameOver) return;
+
+        // alert(e.code);
+if ("KeyA" <= e.code && e.code <= "KeyZ"){
+    if(col < width) {
+       let currentTile = document.getElementById(row.toString() + "-" + col.toString()); 
+if (currentTile.innerText == "") {
+    currentTile.innerText = e.code[3]
+    col++;
+}
+    }
+}
+else if (e.code =="Backspace"){
+    if (0 < col && col <= width){
+        col --;
+    }
+    let currentTile = document.getElementById(row.toString() + "-" + col.toString()); 
+    currentTile.innerText="";
+
+}
+else if (e.code == "Enter"){
+    update();
+    row += 1; //start new row
+    col = 0; //start at 0 for new row 
+}
+if (!gameOver && row == height){
+    gameOver = true;
+    document.getElementById("answer").innerText = word;
+}
+    })
+}
+
+function update(){
+    let correct = 0;
+    for (let c = 0 < width; c++) {
+        let currentTile = document.getElementById(row.toString() + "-" + col.toString()); 
+    currentTile.innerText="";
+    let letter = currentTile.innerText;
+
+    //Is it in the correct position?
+    if (word[c] == letter){
+        tile.classList.add("correct");
+        correct += 1;
+    } // Is it in the word?
+    else if (word.includes(letter)) {
+        tile.classList.add("present");
+    } // Not in the word
+    else ()
     }
 }
